fa5691a1425ececfdee5e0e028c6555da6f0a38f
Oleksii Symon <usymas@gmail.com>
2024-06-03 11:13:59 +0300

    Added `veml6030` subproject

---
+++ veml6030/lib/veml6030.ex
@@ -0,0 +1,18 @@
+defmodule Veml6030 do
+  @moduledoc """
+  Documentation for `Veml6030`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Veml6030.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ veml6030/mix.exs
@@ -0,0 +1,29 @@
+defmodule Veml6030.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :veml6030,
+      version: "0.1.0",
+      elixir: "~> 1.16",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:circuits_i2c, "~> 0.3.9"},
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
